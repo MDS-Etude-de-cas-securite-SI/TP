8103950170175c523ef937d1e1a53e19f5f6a159
Cédric B <cedricbardaine@gmail.com>
2021-03-02 11:52:26 +0100

    add nodejs server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+const app = express();
+
+router.get('/', (req, res) => {
+  res.send('ACCUEIL');
+});
+
+app.use('/', router);
+
+app.listen(process.argv[2] || process.env.PORT || 3000, ()=> {
+  console.log('Le serveur est en écoute sur ${process.argv[2] || process.env.PORT || 3000}');
+});
+
